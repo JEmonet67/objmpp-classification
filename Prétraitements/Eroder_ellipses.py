f3d891baaa85f662083130a8038d5376e270f2d1
JEmonet <jerome.emonet.67@gmail.com>
2020-03-16 15:03:26 +0100

    Ajout du fichier Eroder_ellipses.py

---
+++ Prétraitements/Eroder_ellipses.py
@@ -0,0 +1,9 @@
+#!/home/jerome/anaconda3/bin/python3
+
+"""Written by Xavier Descombes, INRIA"""
+
+import numpy as np
+import math
+import imageio as io
+import matplotlib.pyplot as plt
+from skimage import img_as_ubyte
