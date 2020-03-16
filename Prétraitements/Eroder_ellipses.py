6e5d2f2b1bb961a6f8314d30f6c0170ee2d2b06b
JEmonet <jerome.emonet.67@gmail.com>
2020-03-16 15:50:26 +0100

    Premier jet de Eroder Ellipse pour l'Exemple Type

--- Prétraitements/Eroder_ellipses.py
+++ Prétraitements/Eroder_ellipses.py
@@ -3,7 +3,29 @@
 """Written by Xavier Descombes, INRIA"""
 
 import numpy as np
-import math
 import imageio as io
-import matplotlib.pyplot as plt
 from skimage import img_as_ubyte
+import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+
+def erode_ellipses(path_file, path_output):
+    list_img = [f for f in listdir(path_file) if isfile(join(path_file, f))]
+    n_img = 1
+    somme = np.zeros([2048,2048])
+    
+    for fichier in list_img:
+        img_ellipse = plt.imread(f"{path_file}/{fichier}")
+        img_ellipse[img_ellipse > 0.3] = 0
+        img_ellipse[img_ellipse != 0] = 1
+        somme += img_ellipse
+        io.imwrite(f"{path_output}/{fichier}",img_as_ubyte(img_ellipse))
+    
+    io.imwrite(f"{path_output}/ALL_Ellipses.png",img_as_ubyte(somme))
+
+
+    
+
+path_file_t = "/home/jerome/Stage_Classif_Organoid/Result_MPP/Exemple_type/local_map_GFP-01_w24-DAPI_TIF_2020y03m06d14h55m39s760l"
+path_output_t = "/home/jerome/Stage_Classif_Organoid/Result_MPP/Exemple_type/local_map_GFP-01_w24-DAPI_TIF_2020y03m06d14h55m39s760l/Ellipses_érodée"
+erode_ellipses(path_file_t,path_output_t)
